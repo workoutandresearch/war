1cbed79afc27efbfeb93af5a6edddd0fef51e10b
unknown <workoutandresearch@gmail.com>
2023-11-07 14:03:41 -0500

    new font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,7 +10,7 @@ import type { AppProps } from 'next/app'
 import { ChakraProvider, extendTheme } from "@chakra-ui/react"
 
 import 'styles/globals.css'
-import "@fontsource/orbitron"
+import '@fontsource/share-tech-mono';
 
 const queryClient = new QueryClient()
 
--- pages/optin.tsx
+++ pages/optin.tsx
@@ -218,5 +218,3 @@ export default function Optin() {
     </Box>
   );
 }
-
-
