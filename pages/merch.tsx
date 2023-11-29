ee86a676d870f516f138ca44feb70797458a06f1
unknown <workoutandresearch@gmail.com>
2023-11-29 15:41:07 -0500

    merch

--- pages/merch.tsx
+++ pages/merch.tsx
@@ -47,7 +47,7 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import CustomLogo from 'components/BookPress';
-import Checkout from 'components/Checkout.js'; // Use the correct relative path
+import Checkout from '../components/Checkout';
 
 export default function Merch() {
 
