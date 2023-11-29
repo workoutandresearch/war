66b7c508ab89ea30f862410ee14c9eefd069a8eb
unknown <workoutandresearch@gmail.com>
2023-11-29 16:17:30 -0500

    merch update

--- pages/merch.tsx
+++ pages/merch.tsx
@@ -47,7 +47,7 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import CustomLogo from 'components/BookPress';
-import Checkout from '../components/Checkout';
+import Checkout from '../components/Checkout.js';
 
 export default function Merch() {
 
