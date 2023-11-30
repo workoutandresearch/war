fb48883a4d057048e1956147d291bd90009448ee
unknown <workoutandresearch@gmail.com>
2023-11-30 08:03:38 -0500

    merch update

--- components/checkout.js
+++ components/checkout.js
@@ -1,22 +1,14 @@
 import algosdk from 'algosdk';
 import { algodClient } from 'lib/algodClient';
-import error from 'next/error';
 
-// Define types for your function parameters as needed
-interface CartItem {
-  price: number;
-  quantity: number;
-}
-
-// Assuming getCartItems and getWarPrice are related functions, let's keep them together
-const getCartItems = (): CartItem[] => {
+const getCartItems = () => {
   if (typeof window !== "undefined") {
     return JSON.parse(localStorage.getItem('cart') || '[]');
   }
   return [];
 };
 
-const getWarPrice = (usdPrice: number, usdToAlgoRate: number, algoToWarRate: number): number => {
+const getWarPrice = (usdPrice, usdToAlgoRate, algoToWarRate) => {
   if (!usdToAlgoRate || !algoToWarRate) {
     throw new Error('Invalid rate values');
   }
@@ -24,19 +16,18 @@ const getWarPrice = (usdPrice: number, usdToAlgoRate: number, algoToWarRate: num
   return Math.floor(warPrice);
 };
 
-// Define types for the parameters of sendWarTransaction
-const sendWarTransaction = async (amount: number, activeAddress: string, signTransactions: (transactions: Uint8Array[]) => Promise<{ blob: Uint8Array }>) => {
+const sendWarTransaction = async (amount, activeAddress, signTransactions) => {
   try {
-    const projectAddress = "6KD7NSIJGA3ONUX4TPIQ3TCRDM3Q4HMW53QZOFVD5NIDW4WNZ3L2MF23MY"; // Replace with your project's address
+    const projectAddress = "6KD7NSIJGA3ONUX4TPIQ3TCRDM3Q4HMW53QZOFVD5NIDW4WNZ3L2MF23MY";
     const params = await algodClient.getTransactionParams().do();
     const txn = algosdk.makeAssetTransferTxnWithSuggestedParams(
-      activeAddress, // Sender's address
-      projectAddress, // Receiver's address
-      undefined, // Close remainder to
-      undefined, // Revocation target
-      amount, // Amount
-      undefined, // Note
-      1015673913, // Replace with your actual WAR token ID
+      activeAddress,
+      projectAddress,
+      undefined,
+      undefined,
+      amount,
+      undefined,
+      1015673913,
       params
     );
 
@@ -45,11 +36,11 @@ const sendWarTransaction = async (amount: number, activeAddress: string, signTra
     console.log("Transaction successful with ID: ", sendTx.txId);
   } catch (error) {
     console.error('Error sending WAR transaction:', error);
+    throw error;
   }
 };
 
-// Define types for the parameters of Checkout
-const handleCheckout = async (signTransactions: (transactions: Uint8Array[]) => Promise<{ blob: Uint8Array }>, activeAddress: string, usdToAlgoRate: number, algoToWarRate: number) => {
+const Checkout = async (signTransactions, activeAddress, usdToAlgoRate, algoToWarRate) => {
   try {
     const cartItems = getCartItems();
     const totalWar = cartItems.reduce((total, item) => total + getWarPrice(item.price, usdToAlgoRate, algoToWarRate) * item.quantity, 0);
@@ -57,7 +48,6 @@ const handleCheckout = async (signTransactions: (transactions: Uint8Array[]) => 
     await sendWarTransaction(totalWar, activeAddress, signTransactions);
   } catch (error) {
     console.error('Error in Checkout process:', error);
-    // Handle or display error appropriately
   }
 };
 
