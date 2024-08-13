e3a474aabe39d2989a1e6e1888f8aeac4d73f91c
unknown <workoutandresearch@gmail.com>
2024-08-12 23:12:41 -0400

    ad  update

--- components/BookPress.js
+++ components/BookPress.js
@@ -9,8 +9,8 @@ const CustomLogo = ({ icon, onClick, ariaLabel, variant, color }) => {
   const [isDarkModeLogo, setIsDarkModeLogo] = useState(false);
 
   // Define different URLs for your logo images for light and dark modes
-  const lightModeLogoUrl = 'https://media.discordapp.net/attachments/999962417163419701/1240719973064769678/Untitled_Artwork.png?ex=66479605&is=66464485&hm=01675692a690289e6452249a26b683727de2301b8f417cc1e53fe1ef00b7dfae&=&format=webp&quality=lossless&width=668&height=668';
-  const darkModeLogoUrl = 'https://media.discordapp.net/attachments/999962417163419701/1240719973064769678/Untitled_Artwork.png?ex=66479605&is=66464485&hm=01675692a690289e6452249a26b683727de2301b8f417cc1e53fe1ef00b7dfae&=&format=webp&quality=lossless&width=668&height=668';
+  const lightModeLogoUrl = 'https://cdn.discordapp.com/attachments/999962417163419701/1240719973064769678/Untitled_Artwork.png?ex=66bb9a05&is=66ba4885&hm=31fb73522a4e3c25a16e18b2bc9a3e8982e4919836b43fdc81b2a32d217b82f0&';
+  const darkModeLogoUrl = 'https://cdn.discordapp.com/attachments/999962417163419701/1240719973601644544/Untitled_Artwork.png?ex=66bb9a05&is=66ba4885&hm=250733df1cafe286f3e915b2184ad0d6391ebd02f362bbb5a3b151a481d840ab&';
 
   // Determine the appropriate URL based on the current color mode and the manual toggle
   const logoUrl = isDarkModeLogo ? darkModeLogoUrl : lightModeLogoUrl;
